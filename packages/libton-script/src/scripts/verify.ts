27292825639f2580c1754fb36e3bf7dd6391b2c0
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2019-07-28 12:28:47 +0430

    feat(libton-script): add eslint to verify script

--- packages/libton-script/src/scripts/verify.ts
+++ packages/libton-script/src/scripts/verify.ts
@@ -1,9 +1,32 @@
+import { CLIEngine } from 'eslint';
+import { paths } from '../config/paths';
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err;
 });
+
+function lint() {
+  const cliEngine = new CLIEngine({
+    baseConfig: {
+      extends: ['react-app'],
+      settings: {
+        react: { version: 'latest' },
+      },
+    },
+    cwd: paths.libRoot,
+  });
+  const report = cliEngine.executeOnFiles(['src/**/*.{js,mjs,jsx,ts,tsx}']);
+  const formatter = cliEngine.getFormatter();
+
+  console.info(formatter(report.results));
+
+  if (report.warningCount > 0 || report.errorCount > 0) {
+    process.exit(1);
+  }
+}
 
 function help() {
   console.log(`
@@ -14,7 +37,7 @@ function help() {
     show this message
   
   libton-script verify [script|scripts]
-    run related scripts.
+    run related scripts. allowed scripts: lint
   `);
 }
 
@@ -22,7 +45,7 @@ function verify() {
   console.log("let's clean the cups ☕");
 
   const args = process.argv.slice(3);
-  const allowed = ['help'];
+  const allowed = ['help', 'lint'];
   args.forEach(arg => {
     if (!allowed.includes(arg)) {
       console.error(
@@ -34,11 +57,15 @@ function verify() {
   });
   if (args.length === 0) {
     // run all
+    lint();
     return;
   }
   if (args.includes('help')) {
     help();
     return;
+  }
+  if (args.includes('lint')) {
+    lint();
   }
 }
 
