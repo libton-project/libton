ef3c702055a7389f9e98c3f4107891b77a2c44cb
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2019-09-29 19:08:54 +0330

    fix(create-libton): add doc to generated index.ts file

--- packages/create-libton/generator/template/src/index.ts
+++ packages/create-libton/generator/template/src/index.ts
@@ -1,3 +1,28 @@
+// read more about doc comment syntax in https://api-extractor.com/pages/tsdoc/doc_comment_syntax/
+
+/**
+ * <%= description %>
+ *
+ * @remarks
+ * provide {@link <%= umdName %>} function that returns `<%= filename %>` string
+ *
+ * @packageDocumentation
+ */
+
+/**
+ *
+ * return `<%= filename %>` string
+ *
+ * @returns `<%= filename %>`
+ *
+ * @example
+ * Here's an example:
+ *
+ * ```ts
+ * // Prints "<%= filename %>":
+ * console.log(<%= umdName %>());
+ * ```
+ */
 const <%= umdName %> = () => '<%= filename %>';
 
 export default <%= umdName %>;
