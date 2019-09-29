8925fb23d226365b3a0d4908ebe3d31900da7b51
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2019-09-29 20:33:45 +0330

    docs(share): add and generate api doc

--- packages/libton-share/src/index.ts
+++ packages/libton-share/src/index.ts
@@ -1,1 +1,11 @@
+/**
+ * shared logic for libton
+ *
+ * @remarks
+ *
+ * provide name utils functions {@link getName}, {@link getCliName}, {@link getFilename}
+ *
+ * @packageDocumentation
+ */
+
 export { getName, getCliName, getFilename } from './name.utils';
--- packages/libton-share/src/name.utils.ts
+++ packages/libton-share/src/name.utils.ts
@@ -1,19 +1,66 @@
 import camelcase from 'camelcase';
 import decamelize from 'decamelize';
-// TODO refactor: create and share core logic
 
+/**
+ *
+ * normalize packageName
+ *
+ * @param packageName
+ *
+ * @returns remove `@` and replace `/` with `-`
+ */
 function normalizeName(packageName: string) {
   return packageName.replace('@', '').replace('/', '-');
 }
 
+/**
+ *
+ * convert package name to file name
+ *
+ * @param packageName
+ * @returns normalized and kebab-cased packageName
+ *
+ * @example
+ *
+ * ```ts
+ * getFilename('my.lib') // => my-lib
+ * getFilename('@my-scope/my-lib') // => my-scope-my-lib
+ * ```
+ *
+ */
 export function getFilename(packageName: string) {
   return decamelize(camelcase(normalizeName(packageName)), '-');
 }
 
+/**
+ * convert package name to umd variable name
+ *
+ * @param packageName
+ * @returns normalized and PascalCased packageName
+ *
+ * @example
+ * ```ts
+ * getName('my.lib') // => MyLib
+ * getName('@my-scope/my-lib') // => MyScopeMyLib
+ * ```
+ */
 export function getName(packageName: string) {
   return camelcase(normalizeName(packageName), { pascalCase: true });
 }
 
+/**
+ * convert package name to cli name
+ *
+ * @param packageName
+ * @returns normalized and kebab-cased packageName
+ *
+ * @example
+ *
+ * ```ts
+ * getCliName('my.lib') // => my-lib
+ * getCliName('@my-scope/my-lib') // => my-scope-my-lib
+ * ```
+ */
 export function getCliName(packageName: string) {
   return decamelize(camelcase(normalizeName(packageName)), '-');
 }
