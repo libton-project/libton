8215fd35d002a4f659beeee64f3cae5c89afa10a
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2020-06-10 23:35:55 +0430

    fix: remove commander-remaining-args

--- packages/libton-script/src/commands/libton-test/libton-test.command.ts
+++ packages/libton-script/src/commands/libton-test/libton-test.command.ts
@@ -1,14 +1,13 @@
 import commander from 'commander';
 import { libtonTest, LibtonTestOptions } from './libton-test';
-import getRemainingArgs from 'commander-remaining-args';
 
 export function libtonTestCommand(program: commander.Command) {
   program
     .description('run tests')
     .allowUnknownOption()
     .arguments('[jest-options...]')
-    .action((otherOptions, options: LibtonTestOptions) => {
-      const jestOptions = getRemainingArgs(commander);
+    .action((otherOptions, options: LibtonTestOptions & { args: string[] }) => {
+      const jestOptions = options.args;
       libtonTest(jestOptions, options);
     });
 }
